87e459402965fdf7358d49eabf3c6dfdfeeefb52
okita <t-okita@netoffice.obisan.co.jp>
2021-04-28 16:01:17 +0900

    入力された値で実際にログイン

--- MainMenu/ViewModels/LoginFormViewModel.cs
+++ MainMenu/ViewModels/LoginFormViewModel.cs
@@ -6,12 +6,28 @@ using System.Linq;
 using Prism.Regions;
 using MainMenu.Views;
 using System.Data;
+using System.ComponentModel;
 
 namespace MainMenu.ViewModels
 {
     public class LoginFormViewModel : BindableBase
     {
         private readonly IRegionManager _regionManager;
+
+        string _plainPassword = string.Empty;
+
+        private string _userID = string.Empty;
+        public string UserID
+        {
+            get { return _userID; }
+            set { SetProperty(ref _userID, value); }
+        }
+        private string _loginPassword = string.Empty;
+        public string LoginPassword
+        {
+            get { return _loginPassword; }
+            set { SetProperty(ref _loginPassword, value); }
+        }
 
         public LoginFormViewModel(IRegionManager regionManager)
         {
@@ -22,7 +38,7 @@ namespace MainMenu.ViewModels
         public DelegateCommand LoginCommand { get; }
         private void LoginCommandExecute()
         {
-            if (TryLogin("555", "555"))
+            if (TryLogin(UserID, PlainPassword))
             {
                 // Menu表示
                 _regionManager.RequestNavigate("ContentRegion", nameof(Menu));
@@ -52,6 +68,36 @@ namespace MainMenu.ViewModels
             }
         }
 
+        public string Password
+        {
+            get => new string('*', PlainPassword.Length);
+            set
+            {
+                if (value.Length < PlainPassword.Length)
+                {
+                    PlainPassword = PlainPassword.Substring(0, value.Length);
+                }
+                else if (value.Length > PlainPassword.Length)
+                {
+                    PlainPassword += value.Substring(PlainPassword.Length, value.Length - PlainPassword.Length);
+                }
+                else
+                {
+                    PlainPassword = value;
+                }
 
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Password)));
+            }
+        }
+        public string PlainPassword
+        {
+            get => _plainPassword;
+            set
+            {
+                _plainPassword = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PlainPassword)));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
