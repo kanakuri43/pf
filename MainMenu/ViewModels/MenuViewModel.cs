b47e9c368998fbac3688f5bc7590448347006692
okita <t-okita@netoffice.obisan.co.jp>
2021-04-30 08:52:41 +0900

    Menuをデモ用に修正

--- MainMenu/ViewModels/MenuViewModel.cs
+++ MainMenu/ViewModels/MenuViewModel.cs
@@ -10,7 +10,37 @@ namespace MainMenu.ViewModels
     {
         public MenuViewModel()
         {
+            SalesEntryCommand = new DelegateCommand(SalesEntryCommandExecute);
+            PaymentEntryCommand = new DelegateCommand(PaymentEntryCommandExecute);
+            InvoiceCommand = new DelegateCommand(invoiceCommandExecute);
 
+        }
+        public DelegateCommand SalesEntryCommand { get; }
+        public DelegateCommand PaymentEntryCommand { get; }
+        public DelegateCommand InvoiceCommand { get; }
+        private void SalesEntryCommandExecute()
+        {
+            var p = new System.Diagnostics.Process();
+            var directory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            p.StartInfo.FileName = directory + @"SalesEntry.exe";
+            p.Start();
+        }
+        private void PaymentEntryCommandExecute()
+        {
+            var p = new System.Diagnostics.Process();
+            var directory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            p.StartInfo.FileName = directory + @"PaymentEntry.exe";
+            p.Start();
+        }
+        private void invoiceCommandExecute()
+        {
+            var p = new System.Diagnostics.Process();
+            var directory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            p.StartInfo.FileName = directory + @"Invoice.exe";
+            p.Start();
         }
     }
 }
