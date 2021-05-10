1d06cb07a8a230f01da863042cdb4f43fd924ac1
okita <t-okita@netoffice.obisan.co.jp>
2021-05-10 15:51:23 +0900

    売上登録作成中

--- SalesEntry/Models/SalesEntryModel.cs
+++ SalesEntry/Models/SalesEntryModel.cs
@@ -15,14 +15,71 @@ namespace SalesEntry.Models
         {
             base.OpenConnection();
         }
-        public bool ExecuteProcedure()
+
+        public int RegistHeader(int slipNo, DateTime slipDate, int customerId, int staffId, int operatorId)
         {
-            using (MySqlCommand command = new MySqlCommand(SQL, Connection))
+            using (MySqlCommand command = new MySqlCommand("usp_sales_header_entry", Connection))
             {
                 command.CommandType = CommandType.StoredProcedure;
-                MySqlDataReader reader = command.ExecuteReader();
-                return (reader != null);
+                command.Parameters.AddWithValue("@arg_slip_no", slipNo);
+                command.Parameters.AddWithValue("@arg_slip_date", slipDate);
+                command.Parameters.AddWithValue("@arg_customer_id", customerId);
+                command.Parameters.AddWithValue("@arg_staff_id", staffId);
+                command.Parameters.AddWithValue("@arg_operator_id", operatorId);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        return (int)reader["arg_slip_no"];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
             }
+        }
+        public int RegistDetail(int slipNo, int lineNo, int product_id, int qty, int unitId, int catalogPrice, int unitPrice, int unitCost, int lineTaxPrice, int lineIncludeTaxPrice, int operatorId)
+        {
+            using (MySqlCommand command = new MySqlCommand("usp_sales_detail_entry", Connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@arg_slip_no", slipNo);
+                command.Parameters.AddWithValue("@arg_line_no", lineNo);
+                command.Parameters.AddWithValue("@arg_product_id", product_id);
+                command.Parameters.AddWithValue("@arg_qty", qty);
+                command.Parameters.AddWithValue("@arg_unit_id", unitId);
+                command.Parameters.AddWithValue("@arg_catalog_price", catalogPrice);
+                command.Parameters.AddWithValue("@arg_unit_price", unitPrice);
+                command.Parameters.AddWithValue("@arg_unit_cost", unitCost);
+                command.Parameters.AddWithValue("@arg_line_tax_price", lineTaxPrice);
+                command.Parameters.AddWithValue("@arg_line_include_tax_price", lineIncludeTaxPrice);
+                command.Parameters.AddWithValue("@arg_operator_id", operatorId);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        return (int)reader["arg_slip_no"];
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        public bool DeleteHeader(int slipNo)
+        {
+
+            return true;
+        }
+        public bool DeleteDetail(int slipNo)
+        {
+
+            return true;
         }
 
 
--- SalesEntry/ViewModels/SalesEntryViewModel.cs
+++ SalesEntry/ViewModels/SalesEntryViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
+using System.Windows.Input;
 
 namespace SalesEntry.ViewModels
 {
@@ -23,14 +24,21 @@ namespace SalesEntry.ViewModels
 
         public int SlipNo { get; set; }
         public DateTime SlipDate { get; set; }
-        public int OderSlipNo { get; set; }
         public string CustomerCode { get; set; }
         public string CustomerName { get; set; }
         public int SalesTaxRateId { get; set; }
+        public string ZipCode { get; set; }
+        public string Address { get; set; }
+        public string Tel { get; set; }
+        public string Subtotal { get; set; }
+        public string SalesTax { get; set; }
+        public int Total { get; set; }
 
+        public DataTable SalesDataTable = new DataTable();
 
         public DelegateCommand PrintCommand { get; }
         public DelegateCommand EntryCommand { get; }
+         
 
         public SalesEntryViewModel()
         {
@@ -38,12 +46,46 @@ namespace SalesEntry.ViewModels
             EntryCommand = new DelegateCommand(EntryCommandExecute);
 
             // 初期値表示
-            SlipNo = 123;
-            SlipDate = DateTime.Today;
-            OderSlipNo = 456;
-            CustomerCode = "100";
-            CustomerName = "オビサン株式会社";
+            var m = new Models.SalesEntryModel();
+            m.SQL = "SELECT "
+                  + "  h.id "
+                  + "  , h.slip_no "
+                  + "  , h.slip_date "
+                  + "  , customers.customer_code "
+                  + "  , customers.customer_name "
+                  + "  , customers.zip_code "
+                  + "  , customers.address1 +  customers.address1 address"
+                  + "  , customers.tel "
+                  + "  , staffs.staff_code "
+                  + "  , staffs.staff_name "
+                  + "FROM "
+                  + "  sales_header h "
+                  + "  LEFT JOIN customers "
+                  + "    ON h.customer_id = customers.id "
+                  + "    AND customers.state = 0 "
+                  + "  LEFT JOIN staffs "
+                  + "    ON h.staff_id = staffs.id "
+                  + "   AND staffs.state = 0 "
+                  + "  LEFT JOIN sales_detail d "
+                  + "    ON h.slip_no = d.slip_no "
+                  + "    AND d.state = 0 "
+                  + "WHERE "
+                  + "  h.state = 0 "
+                  + "  AND h.slip_no = 2 ";
+            SalesDataTable = m.ReadAsDataTable();
+
+            SlipNo = (int)SalesDataTable.Rows[0]["slip_no"];
+            SlipDate = (DateTime)SalesDataTable.Rows[0]["slip_date"];
+            CustomerCode = SalesDataTable.Rows[0]["customer_code"].ToString();
+            CustomerName = SalesDataTable.Rows[0]["customer_name"].ToString();
             SalesTaxRateId = 4;
+            ZipCode = SalesDataTable.Rows[0]["zip_code"].ToString();
+            Address = SalesDataTable.Rows[0]["address"].ToString();
+            Tel = SalesDataTable.Rows[0]["tel"].ToString();
+            Subtotal = "1,000";
+            SalesTax = "100";
+            Total = 1100;
+
         }
 
         private void PrintCommandExecute()
@@ -53,13 +95,13 @@ namespace SalesEntry.ViewModels
         }
         private void EntryCommandExecute()
         {
+            int slipNo = 0;
             // 登録ボタンを押したときの処理
-            var dc = new Models.SalesEntryModel();
-            dc.SQL = "sp_sales_entry";
-            dc.ExecuteProcedure();
-
+            var m = new Models.SalesEntryModel();
+            slipNo = m.RegistHeader(0, DateTime.Parse("2021/12/31"), 1, 1, 2);
+            slipNo = m.RegistDetail(slipNo, 1, 1, 23, 1, 1000, 900, 500, 0, 0, 2);
+            SlipNo = slipNo;
         }
-
 
     }
 
