77180626fa6d772851f27436d74b80e1c267e74d
okita <t-okita@netoffice.obisan.co.jp>
2021-05-14 08:47:24 +0900

    salesentry更新

---
+++ SalesEntry/Views/SalesDetail.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SalesEntry.Views
+{
+    /// <summary>
+    /// Interaction logic for SalesDetail
+    /// </summary>
+    public partial class SalesDetail : UserControl
+    {
+        public SalesDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
