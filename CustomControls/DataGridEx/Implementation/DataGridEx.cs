f21b6386b7d3849f7ed3233408ed8e2b00c98937
okita <t-okita@netoffice.obisan.co.jp>
2021-05-31 16:41:45 +0900

    カスタムコントロールを表示するところまで

---
+++ CustomControls/DataGridEx/Implementation/DataGridEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControls
+{
+    public class DataGridEx : Control
+    {
+        static DataGridEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DataGridEx), new FrameworkPropertyMetadata(typeof(DataGridEx)));
+        }
+        public DataGridEx()
+        {
+        }
+    }
+}
