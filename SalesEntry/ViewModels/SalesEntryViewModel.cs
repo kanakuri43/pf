6d4e56a19001a83e61376407048b23921ad35747
okita <t-okita@netoffice.obisan.co.jp>
2021-08-17 09:25:51 +0900

    前伝票・次伝票 機能追加

--- SalesEntry/ViewModels/SalesEntryViewModel.cs
+++ SalesEntry/ViewModels/SalesEntryViewModel.cs
@@ -35,6 +35,8 @@ namespace SalesEntry.ViewModels
         {
             SlipSearchCommand = new DelegateCommand<TextBox>(SlipSearchCommandExecute);
             CustomerSearchCommand = new DelegateCommand<TextBox>(CustomerSearchCommandExecute);
+            PrevSlipCommand = new DelegateCommand(PrevSlipCommandExecute);
+            NextSlipCommand = new DelegateCommand(NextSlipCommandExecute);
             PrintCommand = new DelegateCommand(PrintCommandExecute);
             RegistCommand = new DelegateCommand(RegistCommandExecute);
             DeleteCommand = new DelegateCommand(DeleteCommandExecute);
@@ -140,6 +142,8 @@ namespace SalesEntry.ViewModels
         public DelegateCommand<TextBox> SlipSearchCommand { get; }
         public DelegateCommand<TextBox> CustomerSearchCommand { get; }
         public DelegateCommand<DataGrid> ProductSearchCommand { get; }
+        public DelegateCommand NextSlipCommand { get; }
+        public DelegateCommand PrevSlipCommand { get; }
         public DelegateCommand PrintCommand { get; }
         public DelegateCommand RegistCommand { get; }
         public DelegateCommand DeleteCommand { get; }
@@ -249,6 +253,93 @@ namespace SalesEntry.ViewModels
             }
         }
 
+        private void PrevSlipCommandExecute()
+        {
+            // 前伝票
+            try
+            {
+                var dc = new DatabaseController();
+                if (SlipNo == 0)
+                {
+                    dc.SQL = "SELECT "
+                            + "  slip_no "
+                            + " FROM "
+                            + "   sales_header "
+                            + " WHERE "
+                            + "   state = 0 "
+                            + " ORDER BY "
+                            + "   slip_no DESC "
+                            + " LIMIT 1";
+                }
+                else
+                {
+                    dc.SQL = "SELECT "
+                            + "  slip_no "
+                            + " FROM "
+                            + "   sales_header "
+                            + " WHERE "
+                            + "   state = 0 "
+                            + "   AND slip_no < " + SlipNo.ToString()
+                            + " ORDER BY "
+                            + "   slip_no DESC "
+                            + " LIMIT 1";
+
+                }
+
+                IDataReader dr = dc.ReadAsDataReader();
+                if (dr == null)
+                {
+                    return;
+                }
+                else
+                {
+                    dr.Read();
+                    CallSlip((int)dr[0]);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void NextSlipCommandExecute()
+        {
+            // 次伝票
+            try
+            {
+                var dc = new DatabaseController();
+                dc.SQL = "SELECT "
+                        + "  slip_no "
+                        + " FROM "
+                        + "   sales_header "
+                        + " WHERE "
+                        + "   state = 0 "
+                        + "   AND slip_no > " + SlipNo.ToString()
+                        + " ORDER BY "
+                        + "   slip_no "
+                        + " LIMIT 1";
+
+                IDataReader dr = dc.ReadAsDataReader();
+                if (dr == null)
+                {
+                    return;
+                }
+                else
+                {
+                    dr.Read();
+                    CallSlip((int)dr[0]);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
         private void PrintCommandExecute()
         {
             // 印刷
@@ -297,6 +388,71 @@ namespace SalesEntry.ViewModels
                 _salesDetails.Add(sd);
             }
         }
+        private void CallSlip(int slipNo)
+        {
+            var dc = new DatabaseController();
+            dc.SQL = "SELECT "
+                    + "  H.id h_id  "
+                    + "  , H.slip_no "
+                    + "  , H.slip_date "
+                    + "  , H.customer_id "
+                    + "  , H.staff_id "
+                    + "  , D.id d_id "
+                    + "  , D.line_no "
+                    + "  , D.product_id "
+                    + "  , D.qty "
+                    + "  , D.catalog_price "
+                    + "  , D.unit_price "
+                    + "  , D.unit_cost "
+                    + "  , D.line_sales_tax_price "
+                    + "  , D.line_include_sales_tax_price "
+                    + "  , customers.customer_code "
+                    + "  , customers.customer_name "
+                    + "  , customers.address1 "
+                    + "  , customers.address2 "
+                    + "  , products.product_code "
+                    + "  , products.product_name "
+                    + "FROM "
+                    + "  sales_header H "
+                    + "  LEFT JOIN sales_detail D "
+                    + "    ON H.slip_no = D.slip_no "
+                    + "    AND D.state = 0 "
+                    + "  LEFT JOIN customers  "
+                    + "    ON H.customer_id = customers.id "
+                    + "  LEFT JOIN products  "
+                    + "    ON D.product_id = products.id "
+                    + "WHERE "
+                    + "  H.state = 0 "
+                    + "  AND H.slip_no = " + slipNo;
+
+            DataTable salesDataTable = dc.ReadAsDataTable();
+
+            // HEADER
+            SlipNo = (int)salesDataTable.Rows[0]["slip_no"];
+            SlipDate = (DateTime)salesDataTable.Rows[0]["slip_date"];
+            CustomerCode = salesDataTable.Rows[0]["customer_code"].ToString();
+            CustomerName = salesDataTable.Rows[0]["customer_name"].ToString();
+            SalesTaxRateId = 0;
+            ZipCode = "";
+            Address = salesDataTable.Rows[0]["address1"].ToString() + salesDataTable.Rows[0]["address2"].ToString();
+            Tel = "";
+
+            // DETAIL
+            SalesDetails.Clear();
+            foreach (DataRow dr in salesDataTable.Rows)
+            {
+                var sd = new SalesDetail();
+                sd.LineNo = (int)dr["line_no"];
+                var p = new Product((int)dr["product_id"]);
+                sd.ProductCode = dr["product_code"].ToString();
+                sd.ProductName = dr["product_name"].ToString();
+                sd.Qty = (int)dr["qty"];
+                sd.UnitCost = (int)dr["unit_cost"];
+                sd.UnitPrice = (int)dr["unit_price"];
+                _salesDetails.Add(sd);
+            }
+        }
+
     }
 
 }
